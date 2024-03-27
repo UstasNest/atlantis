0ab9e4d44e531f5297df9cbbea1a56fd4a2a46e4
Rustam Mulyukov <r.muljukov@gmail.com>
2024-03-27 14:11:33 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,2 @@
-resource "null_resource" "111tgest" {}
-resource "null_resource" "2rr2t" {}
-resource "null_resource" "22tt0jtgrrrt" {}
-resource "null_resource" "22t4t11j11g4" {}
+resource "null_resource" "test" {}
+
